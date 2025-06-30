53f41fc54818bf7c84e5327c1e70fb0da2647e40
server <ChromiumOS-Guy>
2025-07-01 02:26:37 +0300

    changed port to be less common to reduce risk of other processes using it

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@ set -e
 
 # Start the miniserve HTTP server in the background.
 # The '&' sends the process to the background, allowing the script to continue.
-./miniserve-0.31.0-aarch64-unknown-linux-gnu . -p 8000 --disable-indexing &
+./miniserve-0.31.0-aarch64-unknown-linux-gnu . -p 64934 --disable-indexing &
 
 # Set a 'trap' to kill the miniserve server process when this script exits,
 # regardless of whether it exits normally or due to an error.
@@ -17,7 +17,7 @@ webapp-container --app-id="fluffychat-web.chromiumos-guy" \
                  --store-session-cookies \
                  --webapp-name="Fluffychat Web" \
                  --enable-media-hub-audio \
-                 127.0.0.1:8000/index.html \
+                 127.0.0.1:64934/index.html \
                  "$@"
 
 # The 'trap' command ensures that even if webapp-container exits,
