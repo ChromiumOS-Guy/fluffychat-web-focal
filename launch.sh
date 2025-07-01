64759fd00c8a5b82365167aa4261c049ab6c6145
server <ChromiumOS-Guy>
2025-07-01 15:31:25 +0300

    bind to localhost

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@ set -e
 
 # Start the miniserve HTTP server in the background.
 # The '&' sends the process to the background, allowing the script to continue.
-./miniserve-0.31.0-aarch64-unknown-linux-gnu . -p 64934 --disable-indexing &
+./miniserve-0.31.0-aarch64-unknown-linux-gnu . -p 64934 --disable-indexing --interfaces 127.0.0.1 &
 
 # Set a 'trap' to kill the miniserve server process when this script exits,
 # regardless of whether it exits normally or due to an error.
